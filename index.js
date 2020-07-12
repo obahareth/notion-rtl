c7f0bb21a9a9110734261d97ecdbef22e811023f
Dotan <dotan@riseup.co.il>
2020-07-12 15:47:10 +0300

    Add support for list items

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-function getTopLevelBlocksWithoutDirAttribute() {
-  return document.querySelectorAll('.notion-page-content > div[data-block-id]:not([dir])')
+
+function alignListItemsToRight() {
+  const items = getListItems();
+
+  items.forEach((item) => {
+    if (isRTL(item.innerText)) {
+      item.style['text-align'] = 'right';
+    }
+  });
+}
+
+function getListItems() {
+  return document.querySelectorAll("div[placeholder='List']");
+}
+
+function isRTL(s){
+  const ltrChars = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF'+'\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF';
+  const rtlChars = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC';
+  const rtlDirCheck = new RegExp('^[^'+ltrChars+']*['+rtlChars+']');
+
+  return rtlDirCheck.test(s);
 }
 
 function setBlocksDirectionToAuto() {
-  let blocks = getTopLevelBlocksWithoutDirAttribute()
+  const blocks = getTopLevelBlocksWithoutDirAttribute();
 
   blocks.forEach((block) => {
     block.setAttribute("dir", "auto")
   })
 }
 
-function notionPageContentIsLoaded() {
-  if (document.querySelector('div.notion-page-content') !== null) {
-    return true
-  }
-
-  return false
+function getTopLevelBlocksWithoutDirAttribute() {
+  return document.querySelectorAll('.notion-page-content > div[data-block-id]:not([dir])')
 }
 
-let pageLoadInterval = setInterval(() => {
+function notionPageContentIsLoaded() {
+  return (document.querySelector('div.notion-page-content') !== null);
+}
+
+const pageLoadInterval = setInterval(() => {
   if (notionPageContentIsLoaded()) {
-    setBlocksDirectionToAuto()
+    setBlocksDirectionToAuto();
+    alignListItemsToRight();
 
     // TODO: If we can find a cleaner way to handle block additions/removals
     // then clear out this interval
-    // clearInterval(pageLoadInterval)  
+    // clearInterval(pageLoadInterval)
   }
-}, 200)
+}, 200);
