6b35caf94f1fc08210bf82072277099a46fd8111
Fahmi Bnchi <fahmibnchi@gmail.com>
2020-08-15 00:44:08 +0300

    Migrate to mutation observer

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const OBSERVER_CONFIG = { childList: true, subtree: true }
+
+// We need to know if the notion document is fully loaded in order
+// to observe any newly added blocks under notion-content-page element
+const NOTION_DOCUMENT_OBSERVER = new MutationObserver(onNotionDocumentLoaded)
+NOTION_DOCUMENT_OBSERVER.observe(document, OBSERVER_CONFIG)
 
 function alignListItemsToRight() {
   const items = getListItems()
@@ -23,17 +29,28 @@ function getTopLevelBlocksWithoutDirAttribute() {
   return document.querySelectorAll('.notion-page-content > div[data-block-id]:not([dir])')
 }
 
-function notionPageContentIsLoaded() {
-  return (document.querySelector('div.notion-page-content') !== null)
+function isNotionPageContentLoaded(node) {
+  if (typeof node !== "undefined") {
+    return node.className === 'notion-page-content'
+  }
+
+  return false;
 }
 
-const pageLoadInterval = setInterval(() => {
-  if (notionPageContentIsLoaded()) {
-    setBlocksDirectionToAuto()
-    alignListItemsToRight()
+function onNotionPageContentChanged() {
+  setBlocksDirectionToAuto()
+  alignListItemsToRight()
+}
 
-    // TODO: If we can find a cleaner way to handle block additions/removals
-    // then clear out this interval
-    // clearInterval(pageLoadInterval)
+function onNotionDocumentLoaded(mutationsList) {
+  for (const mutation of mutationsList) {
+    if (isNotionPageContentLoaded(mutation.addedNodes[0])) {
+      NOTION_DOCUMENT_OBSERVER.disconnect()
+
+      const $notionPageContent = document.querySelector(`.notion-page-content`)
+
+      const pageContentObserver = new MutationObserver(onNotionPageContentChanged)
+      pageContentObserver.observe($notionPageContent, OBSERVER_CONFIG)
+    }
   }
-}, 200)
+}
